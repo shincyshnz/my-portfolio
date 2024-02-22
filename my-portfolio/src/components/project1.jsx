d325bafd67d567482e54da6d143fda277eb45a6c
Shincy <shincyshnz@gmail.com>
2024-02-22 09:57:05 +0400

    initial commit for components

---
+++ my-portfolio/src/components/project1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const project1 = () => {
+  return (
+    <div>project1</div>
+  )
+}
+
+export default project1
---
+++ my-portfolio/src/components/project2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const project2 = () => {
+  return (
+    <div>project2</div>
+  )
+}
+
+export default project2

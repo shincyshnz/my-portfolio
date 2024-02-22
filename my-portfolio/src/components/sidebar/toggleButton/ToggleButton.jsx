c859244743c6191cbc92b818712452eceb9151ac
Shincy <shincyshnz@gmail.com>
2024-02-22 21:53:52 +0400

    hamburger icon in progress

--- my-portfolio/src/components/sidebar/toggleButton/ToggleButton.jsx
+++ my-portfolio/src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,27 +1,27 @@
-import  {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const ToggleButton = ({ setOpen }) => {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
       <svg width="23" height="23" viewBox="0 0 23 23">
-        <motion.path 
-        strokeWidth="3" 
-        stroke="black" 
-        strokeLinecap="round" 
-        variants={{
-          closed : {d: "M 2 2.5 L 20 2.5"},
-          open : {d: "M 3 16.5 L 17 2.5"},
-        }} 
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }}
         />
         <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
-        <motion.path 
-        strokeWidth="3" 
-        stroke="black" 
-        strokeLinecap="round" 
-        variants={{
-          closed : {d: "M 2 2.5 L 20 2.5"},
-          open : {d: "M 3 16.5 L 17 2.5"},
-        }} 
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }}
         />
       </svg>
     </button>

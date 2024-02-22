0d986897b1e4fa112eb4ac96f63a5c5b22737916
Shincy <shincyshnz@gmail.com>
2024-02-22 17:09:42 +0400

    navbar and sidebar created

--- my-portfolio/src/components/navbar/Navbar.jsx
+++ my-portfolio/src/components/navbar/Navbar.jsx
@@ -1,28 +1,46 @@
-import { easeOut, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import "./navbar.scss"
 import { SOCIAL_LINKS } from '../../constants'
+import Sidebar from '../sidebar/Sidebar'
+
+
 
 const Navbar = () => {
   const variants = {
     hidden: { opacity: 0 },
     visible: {
       opacity: 1,
-      transition: { duration: 1 }
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      }
     },
   }
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: -100 },
+    visible: { opacity: 1, y: 0 },
+  }
+
   return (
     <div className="navbar">
       {/* Sidebar */}
+      <Sidebar />
+
+      {/* Logo and Social Links */}
       <div className="wrapper">
         <motion.span
           variants={variants}
           initial="hidden"
           animate="visible"
         >Shincy</motion.span>
-        <motion.ul className="social" initial="hidden" animate="visible" variants={variants}>
+        <motion.ul
+          className="social"
+          initial="hidden"
+          animate="visible"
+          variants={variants}>
           {SOCIAL_LINKS.map((item, i) => (
-            <motion.li key={i}
-              variants={variants}
+            <motion.li key={i} variants={variants}
             >
               <a href={item.url}><img src={item.img} alt={item.alt} /></a>
             </motion.li>

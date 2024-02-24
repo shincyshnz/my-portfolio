59e5b5da7ca03bb7c933d1b09537398fb5984136
Shincy <shincyshnz@gmail.com>
2024-02-24 08:36:23 +0400

    sidebar color link changes on selection

--- my-portfolio/src/components/sidebar/links/Links.jsx
+++ my-portfolio/src/components/sidebar/links/Links.jsx
@@ -1,7 +1,10 @@
-import { motion } from 'framer-motion'
+import { motion, useAnimate } from 'framer-motion'
 import { SIDEBAR_LINKS } from "../../../constants"
+import { useEffect, useState } from 'react'
 
 const Links = () => {
+  const [selected, setSelected] = useState('list--0');
+
   const variants = {
     open: {
       transition: {
@@ -27,23 +30,30 @@ const Links = () => {
     }
   }
 
-return (
-  <motion.div
-    variants={variants}
-    className="links"
-  >
-    {SIDEBAR_LINKS.map((item, index) => (
-      <motion.a
-        variants={itemVariants}
-        whileHover={{scale :1.1}}
-        whileTap={{scale :0.95}}
-        href={`${item.link}`}
-        key={index}>
-        {item.text}
-      </motion.a>
-    ))}
-  </motion.div>
-)
+  const handleClick = (e) => {
+    setSelected(prev => e.target.classList.value);
+  }
+
+  return (
+    <motion.div
+      variants={variants}
+      className="links"
+    >
+      {SIDEBAR_LINKS.map((item, index) => (
+        <motion.a
+          onClick={handleClick}
+          className={`list--${index}`}
+          animate={{ color : `${selected === `list--${index}` ? "#444e5e" : "#000000"}`,}}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          href={`${item.link} `}
+          key={index}>
+          {item.text}
+        </motion.a>
+      ))}
+    </motion.div>
+  )
 }
 
 export default Links

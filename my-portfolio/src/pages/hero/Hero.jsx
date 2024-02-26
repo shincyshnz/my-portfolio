994a85e7730d6c8540ff2ba5e45935afddd7e6fd
Shincy <shincyshnz@gmail.com>
2024-02-26 09:47:51 +0400

    hero completed

--- my-portfolio/src/pages/hero/Hero.jsx
+++ my-portfolio/src/pages/hero/Hero.jsx
@@ -13,9 +13,45 @@ const Hero = () => {
       transition: {
         duration: 1,
         staggerChildren: 0.1,
-      }
-    }
+      },
+    },
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2, repeat: Infinity,
+      },
+    },
   }
+
+  const imgVariants = {
+    initial: {
+      x: 500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+  }
+
+  const sliderVariants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: "-920%",
+      transition: {
+        repeat: Infinity,
+        repeatType: "mirror",
+        duration: 60,
+      },
+    },
+  }
+
 
   return (
     <div className="hero">
@@ -34,19 +70,34 @@ const Hero = () => {
             <motion.button variants={textVariants}>See the latest works</motion.button>
             <motion.button variants={textVariants}>Contact me</motion.button>
           </motion.div>
-          <motion.img variants={textVariants} src="/scroll.png" alt="" />
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png" alt="" />
         </motion.div>
+
         {/* Sliding Text container */}
-        <div className="slidingTextContainer">
-          Web Developer Front End Developer
-        </div>
+        <motion.div className="slidingTextContainer"
+          variants={sliderVariants}
+          initial="initial"
+          animate="animate">
+          React Express MongoDb Html Sass Nodejs Redux Tailwind
+        </motion.div>
 
         {/* Image container */}
-        <div className="imageContainer">
-          <motion.img width={"120%"} src="/hero2.png" alt="hero" />
-        </div>
+        <motion.div
+          className="imageContainer"
+          variants={imgVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.img
+            variants={imgVariants}
+            width={"120%"} src="/hero2.png" alt="hero" />
+        </motion.div>
+
       </div>
-    </div>
+    </div >
   )
 }
 

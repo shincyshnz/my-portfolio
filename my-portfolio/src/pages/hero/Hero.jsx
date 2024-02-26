d9116888d8caf4314d54613c6f8f2358a9306d90
Shincy <shincyshnz@gmail.com>
2024-02-26 08:38:33 +0400

    hero on progress

--- my-portfolio/src/pages/hero/Hero.jsx
+++ my-portfolio/src/pages/hero/Hero.jsx
@@ -1,18 +1,32 @@
 import './hero.scss'
-import Tilt from 'react-parallax-tilt'
 import { motion } from 'framer-motion'
 
 const Hero = () => {
+
   return (
     <div className="hero">
-        <motion.div
-          className="imageContainer"
-          initial={{ x: 1000 }}
-          animate={{ x: 0, delay: 2,}}
-          transition={{ type: "spring", stiffness: 100 }}
-        >
-          <img src="/hero1.png" alt="hero" />
-        </motion.div>
+      <div className="wrapper">
+
+        {/* Text container */}
+        <div className="textContainer">
+          <h2>Shincy KM</h2>
+          <h1>Web Developer</h1>
+          <div className="buttons">
+            <button>See the latest works</button>
+            <button>Contact me</button>
+          </div>
+          <img src="/scroll.png" alt="" />
+        </div>
+        {/* Sliding Text container */}
+        <div className="slidingTextContainer">
+          Web Developer Front End Developer 
+        </div>
+
+        {/* Image container */}
+        <div className="imageContainer">
+          <motion.img width={"120%"} src="/hero2.png" alt="hero" />
+        </div>
+      </div>
     </div>
   )
 }

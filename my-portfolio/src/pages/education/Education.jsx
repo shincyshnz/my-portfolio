2b35c698ada85687178f181fba388a177ea10041
Shincy <shincyshnz@gmail.com>
2024-03-04 10:21:38 +0400

    education completed

--- my-portfolio/src/pages/education/Education.jsx
+++ my-portfolio/src/pages/education/Education.jsx
@@ -393,96 +393,52 @@ const Education = () => {
         </svg>
       </div>
 
-      <div className="timelineContainer">
-        {/* Text Container */}
+      
+
+      <div className="textContainer">
         <motion.div
-          variants={lineAnimation}
+          variants={contentAnimation}
+          className="item"
           initial="initial"
-          // animate="animate"
           whileInView="animate"
-          className="timeline"
+          whileHover={hoverEffect}
         >
-          <div className="container right">
-            <motion.div
-              variants={contentAnimation}
-              className="content"
-              initial="initial"
-              whileInView="animate"
-              whileHover={hoverEffect}
-            >
-              <motion.h2
-                variants={contentAnimation}
-              >M.Tech
-              </motion.h2>
-              <motion.h5
-                variants={contentAnimation}
-              >Computer Science & Engineering
-              </motion.h5>
-              <motion.h5
-                variants={contentAnimation}
-              >APJ Abdul Kalam Technological University
-              </motion.h5>
+          <motion.h2
+            variants={contentAnimation}
+          >M.Tech
+          </motion.h2>
+          <motion.h5
+            variants={contentAnimation}
+          >Computer Science & Engineering
+          </motion.h5>
+          <motion.h5
+            variants={contentAnimation}
+          >APJ Abdul Kalam Technological University
+          </motion.h5>
+        </motion.div>
 
-              {/* <!-- SVG for left container --> */}
-              <motion.svg id="progress" width="100" height="100" viewBox="0 0 100 100">
-                <circle
-                  cx="50"
-                  cy="50"
-                  r="30"
-                  pathLength="1" className="bg"
-                />
-                <motion.circle
-                  cx="50"
-                  cy="50"
-                  r="30"
-                  pathLength="1"
-                  className="indicator"
-                />
-              </motion.svg>
-
-            </motion.div>
-          </div>
-
-          <div className="container right">
-            <motion.div
-              variants={contentAnimation}
-              className="content"
-              initial="initial"
-              whileInView="animate"
-              whileHover={hoverEffect}
-            >
-              <motion.h2
-                variants={contentAnimation}
-              >B.Tech
-              </motion.h2>
-              <motion.h5
-                variants={contentAnimation}
-              > Computer Science & Engineering
-              </motion.h5>
-              <motion.h5
-                variants={contentAnimation}
-              >Calicut University
-              </motion.h5>
-
-              {/* <!-- SVG for right container --> */}
-              <svg id="progress" width="100" height="100" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
-                <motion.circle
-                  cx="50"
-                  cy="50"
-                  r="30"
-                  pathLength="1"
-                  className="indicator"
-                />
-              </svg>
-
-            </motion.div>
-          </div>
+        <motion.div
+          variants={contentAnimation}
+          className="item"
+          initial="initial"
+          whileInView="animate"
+          whileHover={hoverEffect}
+        >
+          <motion.h2
+            variants={contentAnimation}
+          >M.Tech
+          </motion.h2>
+          <motion.h5
+            variants={contentAnimation}
+          >Computer Science & Engineering
+          </motion.h5>
+          <motion.h5
+            variants={contentAnimation}
+          >APJ Abdul Kalam Technological University
+          </motion.h5>
         </motion.div>
       </div>
     </div>
-
-
   )
 }
 
